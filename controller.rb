214283510000261702fab9fec9346ec5f68554f4
Colin Tindle <ampersand@underscore.space>
2018-05-08 10:52:20 +0100

    Header text is now a link to /animals/all

--- controller.rb
+++ controller.rb
@@ -3,3 +3,7 @@ require ( 'sinatra/contrib/all')
 
 require_relative('./controllers/animal_controller')
 require_relative('./controllers/customer_controller')
+
+get '/' do 
+  redirect to('/animals/all')
+end
